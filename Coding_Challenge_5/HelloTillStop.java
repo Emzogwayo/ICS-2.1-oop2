81b242d78527f495c19968e678a4d2433a8ef184
Emma Ogwayo <emmaogwayo@gmail.com>
2025-04-29 09:50:12 +0300

    Add Challenge 5: Hello with while loop

---
+++ Coding_Challenge_5/HelloTillStop.java
@@ -0,0 +1,24 @@
+package Coding_Challenge_5;
+import java.util.*;
+
+class HelloTillStop {
+    Scanner sc = new Scanner(System.in);
+
+    void showHello() {
+        String choice; //guard variable
+
+        System.out.println("For Hello type h, anything else to stop");
+        choice = sc.next();//set guard variable
+        while ("h".equals(choice)) {
+            System.out.println("Hello");
+            choice = sc.next();
+            
+        }
+    }
+    public static void main(String args[]) {
+        HelloTillStop h1 = new HelloTillStop();
+        h1.showHello(); // <-- call the method
+    }
+}
+    
+
