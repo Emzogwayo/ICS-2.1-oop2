e5159fc9894a73904423a35adb4c721fbb0a1c7b
Emma Ogwayo <emmaogwayo@gmail.com>
2025-04-29 09:48:10 +0300

    Add Challenge 1: Greeter class

---
+++ Coding_Challenge_1/Greeter.java
@@ -0,0 +1,15 @@
+package Coding_Challenge_1;
+
+public class Greeter {
+    void greet() {
+        System.out.println("Hi there");
+    }
+
+
+
+
+public static void main(String[] args){
+    (new Greeter()).greet();
+}
+
+}
